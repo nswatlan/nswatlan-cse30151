878fcd70e69a82f9fd532c72e276ed7635d32c60
Norah Swatland <nswatlan@nd.edu>
2024-10-10 18:00:26 -0400

    Creating test csv files

--- project1/2sat_solver.py
+++ project1/2sat_solver.py
@@ -7,9 +7,13 @@
 import csv 
 import argparse
 
-
-
-
+def dppl(filename): 
+    pass
 
 if __name__ == "__main__": 
-    pass 
+    #parse command line argument (containing file of wffs in cnf form) to be solved 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str)
+    args = parser.parse_args()
+
+    print(args.filename)
