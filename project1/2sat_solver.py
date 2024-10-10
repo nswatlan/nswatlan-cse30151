f4ea8cc4e87d08ec4a35c166e2c087c98a877ac9
Norah Swatland <nswatlan@nd.edu>
2024-10-10 18:33:56 -0400

    adding more test files/reading in input

--- project1/2sat_solver.py
+++ project1/2sat_solver.py
@@ -10,10 +10,27 @@ import argparse
 def dppl(filename): 
     pass
 
+def csv_to_wff_dict(csv_data): 
+    list_of_probs = []
+    count  = 0
+    for i in range(len(csv_data)):
+        if csv_data[i][0] == 'c': 
+            print(count)
+            print(f"{csv_data[i]}")
+            print(count)
+            count = 0
+        else:
+            count += 1 
+
+
+
 if __name__ == "__main__": 
     #parse command line argument (containing file of wffs in cnf form) to be solved 
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", type=str)
     args = parser.parse_args()
-
-    print(args.filename)
+    #read in wff from csv 
+    with open(args.filename) as file: 
+        CSV_data = list(csv.reader(file))
+        print(len(CSV_data))
+    csv_to_wff_dict(CSV_data)
